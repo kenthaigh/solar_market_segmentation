aef982803953a0909c76f423ae95ac71c252d0a8
kenthaigh <kenthaigh@gmail.com>
2018-09-10 13:41:36 +1000

    Update energymatters_spider.py

--- energymatters_spider.py
+++ energymatters_spider.py
@@ -3,18 +3,18 @@ import re
 
 class EnergySpider(scrapy.Spider):
     name = "energymatters"
-    start_urls = ['https://www.energymatters.com.au/solar-location/williamtown-2318/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/williamtown-2318/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/valencia-creek-3860/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/unley-5061/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/reids-creek-4625/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/oak-flats-2529/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/kangaroo-creek-2460/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/homebush-west-2140/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/gordon-park-4031/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/daintree-4873/']  # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/beaumont-2577/'] # Restart after broken link
-    # start_urls = ['https://www.energymatters.com.au/solar-location/abbotsford-2046/'] # First page
+    start_urls = ['https://www.energymatters.com.au/solar-location/abbotsford-2046/',     # First page
+                  'https://www.energymatters.com.au/solar-location/williamtown-2318/',    # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/williamtown-2318/',    #  Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/valencia-creek-3860/', # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/unley-5061/',          # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/reids-creek-4625/',    # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/oak-flats-2529/',      # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/kangaroo-creek-2460/', # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/homebush-west-2140/',  # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/gordon-park-4031/',    # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/daintree-4873/',       # Restart after broken link
+                  'https://www.energymatters.com.au/solar-location/beaumont-2577/']       # Restart after broken link
 
     def parse(self, response):
         postcode_string = response.xpath('//meta[@property=$val]', val='og:url').extract()
@@ -26,7 +26,3 @@ class EnergySpider(scrapy.Spider):
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
-
-
-
-
